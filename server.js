d297ee5ac7e7a6e38ee51b0474a06c722a64df24
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-06-29 15:24:17 +0530

    added mongoose connections and functions

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
+
+mongoose.connect("mongodb://localhost/subscribers", { useNewUrlParser: true });
+const db = mongoose.connection;
+
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("Connect to Database"));
 
 app.listen(port, () => {
   console.log(`Server running @ : ${port}`);
