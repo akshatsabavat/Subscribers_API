d29e8beea66f549baec044c564df0d040db2163a
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-06-29 15:38:06 +0530

    added URL as an .env variable

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+require("dotenv").config(); //configs and loads all environment variables from the .env file
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-mongoose.connect("mongodb://localhost/subscribers", { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 
-db.on("error", (error) => console.log(error));
-db.once("open", () => console.log("Connect to Database"));
+db.on("error", (error) => console.log(error)); // runs on error
+db.once("open", () => console.log("Connect to Database")); //runs only once when the db connects
+
+app.use(bodyParser.json());
 
 app.listen(port, () => {
   console.log(`Server running @ : ${port}`);
