7f4941e62dc5e88bd81f8358f11342ecd80f19f4
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-06-29 16:58:31 +0530

    post method added to server code

--- models/subscriberModel.js
+++ models/subscriberModel.js
@@ -9,7 +9,7 @@ const subscriberSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  subscribeData: {
+  subscribeDate: {
     type: Date,
     required: true,
     default: Date.now,
--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,9 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const Subscriber = require("../models/subscriberModel");
 
 //Getting all subs
-router.get("/", (req, res) => {
-  res.send({ message: "Hello World" });
+router.get("/", async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find();
+    res.json(subscribers);
+  } catch (err) {
+    res.status(500).json({ message: err.message }); //errCode : 500 --> means something went wrong with the server
+  }
+});
+
+//Creating a subscription
+router.post("/", async (req, res) => {
+  const name = req.body.name;
+  const subscribedToChannel = req.body.subscribedToChannel;
+
+  const subscriber = new Subscriber({
+    name,
+    subscribedToChannel,
+  });
+
+  try {
+    const newSub = await subscriber.save(); //saves all the request inputs into the schema
+    res.status(201).json(newSub); // errCode : 201 --> means an object has been successfully created
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 module.exports = router;
